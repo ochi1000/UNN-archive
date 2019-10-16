c27c262759a7dde670a2dd87fce834ac3fad1714
ochi1000 <39088587+ochi1000@users.noreply.github.com>
2019-10-16 06:06:29 +0100

    touches

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -10,21 +10,20 @@
         <p class="published"><span><i class="fa fa-user-o"></i></span> Published by {{$file->author}}</p>
         <hr>
         <div>
-            <form method="GET" action="/download/{{$file->id}}">
-                <button type="submit" class="btn btn-primary"><span><i class="fa fa-download"></i></span> Download file</button>
+            <form method="GET" action="https://drive.google.com/file/d/10wD59nGEJbUhQfoSiqJueko5DSSHgICi/view?usp=sharing">
+                <button type="submit" class="btn btn-primary"><span><i class="fa fa-download"></i></span> View and Download file</button>
                 <button class="btn btn-light"><span><i class="fa fa-quote-left"></i></span> Cite</button>   
             </form>
         </div>
         <hr>
         <h4>Abstract</h4>
-        <p>{{$file->description}}</p>
+        <p class="abstract">{{$file->description}}</p>
+        <hr>
+        <br>
     </div>
-    
 </section>
 
-<section class="details-image ">
-    <iframe src='https://view.officeapps.live.com/op/embed.aspx?src=http%3A%2F%2Fieee802%2Eorg%3A80%2Fsecmail%2FdocIZSEwEqHFr%2Edoc' width='100%' height='100%' frameborder='0'>This is an embedded <a target='_blank' href='http://office.com'>Microsoft Office</a> document, powered by <a target='_blank' href='http://office.com/webapps'>Office Online</a>.</iframe>
-</section>
+
 
 <section class="mx-auto col-md-7 mt-3 small-screen">
     <div>
