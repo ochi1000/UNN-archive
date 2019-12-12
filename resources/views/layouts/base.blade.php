e1ff55fe9f2c5b5a57bc2fb66e1a2f8a16abd5d4
ochi1000 <39088587+ochi1000@users.noreply.github.com>
2019-12-12 16:53:00 +0100

    gh

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,11 +6,12 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="/css/app.css" />
+    <title>ECE Archive</title>
     <script src="https://use.fontawesome.com/963cbc0c8e.js"></script>
     <script async src="/js/app.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
     <script src="/js/Bootstrap-3-Typeahead-master/bootstrap3-typeahead.min.js"></script>
-   
+
 
 </head>
 
